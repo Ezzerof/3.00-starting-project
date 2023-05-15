71947c627fc4bf722c3e2be67e5f1def07ee3a30
Ezzerof <christian.bitca@gmail.com>
2023-05-15 10:28:56 +0100

    created test for creating grade through post request

--- src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -51,28 +51,26 @@ public class GradebookController {
 	public String studentInformation(@PathVariable int id, Model m) {
 		if (!service.checkIfStudentIsNull(id))
 			return "error";
-		GradebookCollegeStudent studentEntity = service.studentInformation(id);
-		m.addAttribute("student", studentEntity);
-		if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
-			m.addAttribute("mathAverage", studentEntity.getStudentGrades().findGradePointAverage(studentEntity.getStudentGrades().getMathGradeResults()));
-		} else {
-			m.addAttribute("mathAverage", "N/A");
-		}
-
-		if (studentEntity.getStudentGrades().getScienceGradeResults().size() > 0) {
-			m.addAttribute("scienceAverage", studentEntity.getStudentGrades().findGradePointAverage(studentEntity.getStudentGrades().getScienceGradeResults()));
-		} else {
-			m.addAttribute("scienceAverage", "N/A");
-		}
-
-		if (studentEntity.getStudentGrades().getHistoryGradeResults().size() > 0)
-			m.addAttribute("historyAverage", studentEntity.getStudentGrades().findGradePointAverage(studentEntity.getStudentGrades().getHistoryGradeResults()));
-		else {
-			m.addAttribute("historyAverage", "N/A");
-		}
+		service.configureStudentInformationModel(id, m);
 		return "studentInformation";
 	}
 
+	@PostMapping(value = "/grades")
+	public String createGrade(@RequestParam("grade") double grade,
+							  @RequestParam("gradeType") String gradeType,
+							  @RequestParam("studentId") int studentId, Model m) {
+		if (!service.checkIfStudentIsNull(studentId))
+			return "error";
+
+		boolean success = service.createGrade(grade, studentId, gradeType);
+
+		if (!success)
+			return "error";
+
+		service.configureStudentInformationModel(studentId, m);
+
+		return "studentInformation";
+	}
 
 
 }
